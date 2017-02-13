b8ea9806a9c2d32d085b665971142b78d0a7a05c
Adam Carter <adam@jakatasalon.co.uk>
2017-02-13 11:59:56 +0000

    created FreeTimes table and model

---
+++ app/FreeTime.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Carbon\Carbon;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FreeTime extends Model
+{
+    protected $guarded = ['id'];
+    
+    protected $dates = ['created_at', 'updated_at', 'date_regarding'];
+    
+    public function staff()
+    {
+        return $this->belongsTo('App\Staff', 'staff_id');
+    }
+}
